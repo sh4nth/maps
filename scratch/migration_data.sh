e509702f25b67ddc717b3e59bc0c9b8f7c05752b
Shanthanu Bhardwaj <sbhardwaj@palantir.com>
2016-08-11 15:05:25 -0700

    Fix script to not drop numbers with 00 in them o.O

--- scratch/migration_data.sh
+++ scratch/migration_data.sh
@@ -1,4 +1,4 @@
 awk -F',' '{if($6=="\"Total\"" && $7 == "\"Total\""){print $2","$3","$5","$8} }' D02.csv > D02-simple.csv
-grep -v "00" D02-simple.csv | grep -v "^\"07\"" > temp
+grep -v ",\"00\"" D02-simple.csv | grep -v "^\"07\"" > temp
 grep  "^\"07\",\"00" D02-simple.csv >> temp
-sort temp| sed s/" "/_/g | sed s/\"//g  > cleanedUp_migration_data.csv
+sort temp| sed s/" "/_/g | sed s/\"//g | sed s/Uttranchal/Uttaranchal/g | sed s/gargh/garh/g | sed s/Chhatis/Chhattis/g | sed s/"&"/and/g | sed s/A_and_N_Islands/Andaman_and_Nicobar/g > cleanedUp_migration_data.csv
